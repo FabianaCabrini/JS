bd808d30c4d1df3effd2966e1255d744b0988314
Fabiana Cabrini <fabianacabrini04@gmail.com>
2025-10-02 15:44:01 -0300

    saidas por JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
- alert('Olá, usuário');
+// Referenciando um elemento por ID
+document.getElementById("texto").innerHTML="Texto <b>JS</b>!";
+alert(10+5);
+//outra forma de imprimir valor pelo console
+console.log("Oi, tamo testando peolo console")
